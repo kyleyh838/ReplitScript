df7dffd02b9104ae3c4de5cf49251e5e1fc06738
kyleyh838 <64256385+kyleyh838@users.noreply.github.com>
2023-10-08 08:03:25 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,20 @@
+echo "Cloudreve一键搭建脚本"
+echo "GitHub开源地址：https://github.com/kyleyh838/CloudreveReplit"
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O .replit https://github.com/kyleyh838/CloudreveReplit/raw/master/.replit
+wget -O replit.nix https://github.com/kyleyh838/CloudreveReplit/raw/master/replit.nix
+cd ..
+wget -O main.sh https://github.com/kyleyh838/CloudreveReplit/raw/master/main.sh
+releases_url=https://api.github.com/repos/cloudreve/Cloudreve/releases/latest
+tag_name=`wget -qO- $releases_url | grep tag_name | cut -f4 -d "\""`
+wget -O cloudreve.tar.gz https://github.com/cloudreve/Cloudreve/releases/download/${tag_name}/cloudreve_${tag_name}_linux_amd64.tar.gz
+tar -zxvf "cloudreve.tar.gz" --overwrite
+rm "cloudreve.tar.gz" "LICENSE" "README.md" "README_zh-CN.md"
+cp -r build/.replit . && cp -r build/replit.nix .
+rm -rf build/
+echo "恭喜搭建完成"
+echo "Cloudreve ${tag_name}"
+echo "点击Run立即运行项目"
+echo "请注意保存初始账号密码"
