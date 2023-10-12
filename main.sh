9172b67c0c566ebc2a096d7e8f8619e84cc7dc9b
kyleyh838 <64256385+kyleyh838@users.noreply.github.com>
2023-10-12 10:11:28 +0800

    add keep online

--- main.sh
+++ main.sh
@@ -1,1 +1,3 @@
+URL=${REPL_SLUG}.${REPL_OWNER}.repl.co
+while true; do curl -s "https://$URL" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 300; done &
 chmod +x cloudreve && ./cloudreve
